97029f7787cb5d17421354359c48741acea621bd
Eius <34424590+Eius@users.noreply.github.com>
2023-05-04 18:50:04 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import "../app.css";
+	import "$lib/styles/global.css"
+</script>
+
+<div class="app">
+	<main class="bg-gray container">
+		<h2 class="text-orange text-center font-sans text-5xl py-8">Cloudflare template</h2>
+		<slot />
+	</main>
+</div>
+
+<style lang="postcss">
+
+</style>
