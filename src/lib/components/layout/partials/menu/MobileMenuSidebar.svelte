722139104b6581544f35013340d792b96471aded
Eius <34424590+Eius@users.noreply.github.com>
2023-05-06 15:08:33 +0200

    #5

--- src/lib/components/layout/partials/menu/MobileMenuSidebar.svelte
+++ src/lib/components/layout/partials/menu/MobileMenuSidebar.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="block lg:hidden">
-    <div class="bg-gold p-4 z-50 fixed top-0 bottom-0 left-0 right-1/4 md:right-2/4 {$showMobileMenu ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300">
+    <div class="bg-gold p-4 z-50 fixed top-0 bottom-0 left-0 right-1/3 sm:right-2/4 md:right-2/3 {$showMobileMenu ? "translate-x-0" : "-translate-x-[101%]"} transition-transform duration-300">
         <button class="float-right text-4xl" on:click={() => $showMobileMenu = false}>
             <Icon icon="material-symbols:close-rounded" />
         </button>
